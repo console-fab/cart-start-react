5a5ac69f8bde8a6c71e9ecae914127350a3358a2
Treyceratops <treytowers@yahoo.com>
2022-10-22 10:35:15 -0700

    Add css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import About from './components/About'
 import GroceryList from './components/GroceryList'
 
@@ -8,18 +8,26 @@ function App() {
   const [groceryItem, setGroceryItem] = useState([]);
 
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={<About />} />
-        <Route
-        path='/grocery-list' 
-        element={<GroceryList 
-        groceryItem={groceryItem} 
-        setGroceryItem={setGroceryItem} />}
-        />
-      </Routes>
-    </div>
-  );
+		<div className='App'>
+			<div className='wave'></div>
+			<div className='wave wave2'></div>
+			<div className='wave wave3'></div>
+			<div>
+				<Routes>
+					<Route path='/' element={<About />} />
+					<Route
+						path='/grocery-list'
+						element={
+							<GroceryList
+								groceryItem={groceryItem}
+								setGroceryItem={setGroceryItem}
+							/>
+						}
+					/>
+				</Routes>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 function About(props) {
     return (
         <div>
-            <h1>Welcome to Cart-Start 🛒</h1>
-            <p>This essential Grocery List is a life saver on the busy weeks. <br /> Add, view, edit or remove grocery items to your list which are organized into different categories just like your favorite grocery store!</p>
+            <h1 className='welcome'>Welcome to Cart-Start 🛒</h1>
+            <span className='center'>
+            <p className='about bubble'>This essential Grocery List is a life saver on the busy weeks. <br /> Add, view, edit or remove grocery items to your list. We'll do the organizing for you.</p>
             <hr /> 
-            <p>Lovingly created by Ai, Brandon, Morteza and Trey for your convenience</p>
+            <p className='creators'>Lovingly created by Ai, Brandon, Morteza and Trey for your convenience</p>
             <Link to='/grocery-list'>
-				<button className='grocery-link'>Start adding groceries!</button>
+				<button className='grocery-link'>Start Adding Groceries!</button>
 			</Link>
+            </span>
         </div>
     );
 }
