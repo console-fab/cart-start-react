da226b3b35c0b96117cc1ca170526c6c0d26010f
caterada <caterada@gmail.com>
2022-10-25 12:41:59 -0700

    reword about bubble

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About(props) {
         <div>
             <h1 className='welcome'>Welcome to <br /> Cart-Start 🛒</h1>
             <span className='center'>
-            <p className='about bubble'>This essential Grocery List is a life saver on the busy weeks. <br /> Add, view, edit or remove grocery items to your list. We'll do the organizing for you.</p>
+            <p className='about bubble'>This essential Grocery List is a life saver on the busy weeks. <br /> Add, view, edit or remove grocery items to/from your list. We'll do the organizing for you.</p>
             <hr /> 
             <p className='creators'>Lovingly created by Ai, Brandon, Morteza and Trey for your convenience</p>
             <Link to='/grocery-list'>
